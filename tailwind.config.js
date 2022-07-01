ad6dd266a70a3b8e04eaccead077207582973a55
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2022-07-01 17:35:40 -0500

    resizing screens width

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'sm': {'min': '375px', 'max': '767px'},
+        'sm': {'max': '767px'},
         'md': {'min': '768px', 'max': '991px'},
         'lg': {'min': '992px', 'max': '1199px'},
         'xl': {'min': '1200px'},
